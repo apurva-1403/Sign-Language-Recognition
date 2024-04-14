5f915cd6d0f7ccd54067d2a192e9437563a6f750
apurva-1403 <108210483+apurva-1403@users.noreply.github.com>
2024-04-14 23:34:00 +0530

    Update train_classifier.py

--- train_classifier.py
+++ train_classifier.py
@@ -1,10 +1,12 @@
 import pickle
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import numpy as np
+from sklearn.model_selection import cross_val_score, StratifiedKFold
 
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load data from pickle file
 with open('./data.pickle', 'rb') as f:
@@ -20,26 +22,24 @@ data_padded = [np.pad(d, (0, max_length - len(d)), mode='constant')
 # Convert the padded data to a numpy array and flatten it
 data = np.array([np.ravel(np.asarray(d)) for d in data_padded])
 labels = np.array(data_dict['labels'])
-# labels = np.asarray(data_dict['labels'])
 
+# Split data into train and test sets
 x_train, x_test, y_train, y_test = train_test_split(
     data, labels, test_size=0.35, shuffle=True, stratify=labels)
 
+# Initialize and train the model
 model = RandomForestClassifier()
-
 model.fit(x_train, y_train)
 
+
+# Predict labels for test data
 y_predict = model.predict(x_test)
 
-score = accuracy_score(y_predict, y_test)
+# Compute accuracy
+score = accuracy_score(y_test, y_predict)
+print('{}% of samples were classified correctly!'.format(score * 100))
 
-# Compute validation accuracy
-y_val_predict = model.predict(x_test)
-val_score = accuracy_score(y_val_predict, y_test)
-print("Validation accuracy:", val_score*100)
 
-print('{}% of samples were classified correctly !'.format(score * 100))
-
-f = open('model.p', 'wb')
-pickle.dump({'model': model}, f)
-f.close()
+# Save the model
+with open('model.p', 'wb') as f:
+    pickle.dump({'model': model}, f)
